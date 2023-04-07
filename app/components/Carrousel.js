923eaa06af6380660834c6ddded40e1f98b256d9
kazmonroy <katherinemonroy@gmail.com>
2023-04-07 21:02:57 +0200

    Fix image carrousel

--- app/components/Carrousel.js
+++ app/components/Carrousel.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Carrousel() {
   return (
-    <section className='flex py-8 justify-center gap-5 sm:gap-8 z-10 sm:mx-8 sm:border-x-2 border-x-zinc-100/50 dark:border-x-zinc-50/5 bg-white dark:bg-zinc-900'>
+    <section className='flex overflow-auto py-8 justify-center gap-5 sm:gap-8 sm:mx-8 sm:border-x-2 border-x-zinc-100/50 dark:border-x-zinc-50/5 bg-white dark:bg-zinc-900'>
       <div className='relative aspect-[9/10] w-44 flex-none  rounded-xl  dark:bg-zinc-800 sm:w-72 sm:rounded-2xl rotate-2 bg-zinc-100'>
         <Image
           src={'/malmo.jpeg'}
