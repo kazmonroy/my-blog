42464d279172e6f5d22f0ad7c38dd83bbe9d2d0a
kazmonroy <katherinemonroy@gmail.com>
2023-05-31 00:35:18 +0200

    Fix menu toggle

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,14 +1,64 @@
 'use client';
+
 import Link from 'next/link';
-import { useState } from 'react';
+import { RefObject, useEffect, useRef, useState } from 'react';
 import { Menu } from 'react-feather';
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
-  function toggleMenu() {
+  function toggleMenu(e: any) {
+    // e.stopPropagation();
+    // e.nativeEvent.stopImmediatePropagation();
+
     setIsOpen((prevState) => !prevState);
   }
+
+  // useEffect(() => {
+  //   // document.body.addEventListener('click', () => {
+  //   //   if (isOpen) {
+  //   //     setIsOpen(false);
+  //   //   }
+  //   // });
+
+  // });
+
+  // function useOutsideAlerter(ref: RefObject<HTMLButtonElement>) {
+  //   useEffect(() => {
+  //     /**
+  //      * Alert if clicked on outside of element
+  //      */
+  //     function handleClickOutside(event: any) {
+  //       if (ref.current && !ref.current.contains(event.target)) {
+  //         setIsOpen(false);
+  //       }
+  //     }
+  //     // Bind the event listener
+  //     document.addEventListener('mousedown', handleClickOutside);
+  //     return () => {
+  //       // Unbind the event listener on clean up
+  //       document.removeEventListener('mousedown', handleClickOutside);
+  //     };
+  //   }, [ref]);
+  // }
+
+  // const wrapperRef = useRef(null);
+  // useOutsideAlerter(wrapperRef);
+
+  const handleClickOutside = (event: any) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  const ref: any = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside, true);
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true);
+    };
+  }, [ref]);
 
   return (
     <header className='flex py-4 items-center relative'>
@@ -36,23 +86,30 @@ export default function Header() {
         </ul>
 
         <ul
+          ref={ref}
           className={`${
             isOpen
               ? 'absolute backdrop-blur-sm w-screen  opacity-100 top-14 -left-4 py-4 transition-all ease-in-out border-b border-indigo-950'
               : 'absolute  w-screen  opacity-0 -top-80 -left-4 b py-4 transition-all ease-in-out'
-          } flex flex-col gap-6 items-center bg-[#101223]/90`}
+          } flex flex-col gap-6 items-center bg-[#101223]/90 md:hidden`}
         >
           <li className='font-white hover:text-indigo-500 font-light transition-all ease-in-out'>
-            <Link href='/about'>About</Link>
+            <Link onClick={toggleMenu} href='/about'>
+              About
+            </Link>
           </li>
           <li className='font-white hover:text-indigo-500 font-light transition-all ease-in-out'>
-            <Link href='/projects'>Projects</Link>
+            <Link onClick={toggleMenu} href='/projects'>
+              Projects
+            </Link>
           </li>
           <li className='font-white hover:text-indigo-500 font-light transition-all ease-in-out'>
             <Link href='/blog'>Blog</Link>
           </li>
           <li className='font-roboto px-4 py-2 text-cyan-400 backdrop-blur ring-1 ring-cyan-900 font-light rounded-full hover:bg-cyan-500 hover:text-white hover:transition-all'>
-            <Link href='/contact'>Contact</Link>
+            <Link onClick={toggleMenu} href='/contact'>
+              Contact
+            </Link>
           </li>
         </ul>
       </nav>
