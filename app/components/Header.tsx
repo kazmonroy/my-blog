c555404d3c09ea650f88d6bcf42b6f67b225af15
kazmonroy <katherinemonroy@gmail.com>
2023-06-04 01:26:16 +0200

    Fix navbar on mobile

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import { RefObject, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Menu } from 'react-feather';
 
 export default function Header() {
@@ -53,16 +53,13 @@ export default function Header() {
           <li className='font-white hover:text-indigo-500 font-light transition-all ease-in-out'>
             <Link href='/blog'>Blog</Link>
           </li>
-          <li className='font-roboto px-4 py-2 text-cyan-400 backdrop-blur ring-1 ring-cyan-900 font-light rounded-full hover:bg-cyan-500 hover:text-white hover:transition-all'>
-            <Link href='/contact'>Contact</Link>
-          </li>
         </ul>
 
         <ul
           ref={ref}
           className={`${
             isOpen
-              ? 'absolute backdrop-blur-sm w-screen  opacity-100 top-14 -left-4 py-4 transition-all ease-in-out border-b border-indigo-950'
+              ? 'absolute z-10 backdrop-blur-sm w-screen  opacity-100 top-14 -left-4 py-4 transition-all ease-in-out border-b border-indigo-950'
               : 'absolute  w-screen  opacity-0 -top-80 -left-4 b py-4 transition-all ease-in-out'
           } flex flex-col gap-6 items-center bg-[#101223]/90 md:hidden`}
         >
